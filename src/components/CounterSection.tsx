5ef3e551457f0a4440dc1a4e0ae88ae658fdaa1e
Umur Deniz Dikmen <117382440+UmurDenizDikmen@users.noreply.github.com>
2023-11-08 13:49:29 +0300

    updatecountersection

--- src/components/CounterSection.tsx
+++ src/components/CounterSection.tsx
@@ -52,14 +52,14 @@ export default function CounterSection() {
   }, []);
   return (
     <div className="bg-green-500 mb-3 p-5 h-56 w-screen">
-      <div className="flex flex-row xl:gap-x-52 lg:gap-x-3 lg:ml-10 mt-10 gap-x-0">
+      <div className="flex flex-row mt-10 lg:gap-x-8 gap-x-4 md:gap-x-2 md:ml-8 md:mr-6 justify-between">
         {counter.map((counter, index) => (
           <div className="flex flex-row gap-2" key={index}>
             <h2 className="lg:text-6xl text-xl font-bold mb-2">
               {counter.logo}
             </h2>
             <div className="flex flex-col">
-              <p className="text-white font-bold lg:text-3xl xl:text-3xl text-sm md:text-3xl">
+              <p className="text-white font-bold  lg:text-3xl text-sm md:text-3xl">
                 {" "}
                 +{" "}
                 {counterOn ? (
